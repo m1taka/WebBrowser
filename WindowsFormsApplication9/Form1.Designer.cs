f06c670f5b0700eb396b5993ccb6103d231ec6b2
Dimitar <dimitar.lukarski@gmail.com>
2016-04-20 16:19:48 +0300

    Added files via upload

---
+++ WindowsFormsApplication9/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.go = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.forward = new System.Windows.Forms.Button();
+            this.home = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // go
+            // 
+            this.go.Cursor = System.Windows.Forms.Cursors.Default;
+            this.go.Location = new System.Drawing.Point(1197, 10);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(75, 23);
+            this.go.TabIndex = 0;
+            this.go.Text = "GO!";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(12, 38);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 1;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.file_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1179, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // forward
+            // 
+            this.forward.Location = new System.Drawing.Point(93, 38);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(75, 23);
+            this.forward.TabIndex = 3;
+            this.forward.Text = "Forward";
+            this.forward.UseVisualStyleBackColor = true;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // home
+            // 
+            this.home.Location = new System.Drawing.Point(1116, 38);
+            this.home.Name = "home";
+            this.home.Size = new System.Drawing.Size(75, 23);
+            this.home.TabIndex = 4;
+            this.home.Text = "HOME";
+            this.home.UseVisualStyleBackColor = true;
+            this.home.Click += new System.EventHandler(this.home_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(1197, 38);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 5;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 67);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1258, 533);
+            this.webBrowser1.TabIndex = 6;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 603);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.home);
+            this.Controls.Add(this.forward);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.go);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.Button home;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ WindowsFormsApplication9/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void file_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+    }
+}
